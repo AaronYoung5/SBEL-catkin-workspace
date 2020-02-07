b1473fc48412f627e505e118371e5bea839b4a45
Aaron Young <aryoung5@wisc.edu>
2020-02-07 17:53:05 -0600

    Working on docker

--- docker/ros_cuda_docker/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xhost +
-
-docker run --env='DISPLAY' -v /home/aaron:/home/aaron -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v /et    c/shadow:/etc/shadow:ro -v /usr/local/cuda:/usr/local/cuda --runtime=nvidia --network=host --rm --name ros-cuda-docker ros-cuda-docker &
